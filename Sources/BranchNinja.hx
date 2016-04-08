cbb39784af7ffc554ca111ed345bb0bd11712bc7
varomix wey <varomix@gmail.com>
2016-04-07 18:17:44 -0600

    progress on level loading from tiled tmx

--- Sources/BranchNinja.hx
+++ Sources/BranchNinja.hx
@@ -1,5 +1,6 @@
 package;
 
+import haxe.xml.Fast;
 import kha.Framebuffer;
 import kha.Scheduler;
 import kha.System;
@@ -14,13 +15,25 @@ import kha2d.Scene;
 import kha2d.Tilemap;
 import kha2d.Scene;
 
+// import tmx.TmxEntity;
 
+class Layer
+{
 
+    public var state:String;
+    public var volume:Int;
+    public var value:String;
+
+    //And so on, listing all the fields from the XML attributes and values.
+
+    public function new() { }
+}
 
 class BranchNinja {
 
 	public var player:Player;
 	public var bug:Bug;
+
 
 	public function new() {
 		Assets.loadEverything(create);
@@ -31,15 +44,69 @@ class BranchNinja {
 
 	private function create()
 	{
+		// MAP LOADING
+		
+		var world = Assets.blobs.testMap_tmx;
+
+		var xml:Xml = Xml.parse(world.toString());
+		var fast:Fast = new Fast(xml.firstElement());
+		
+		for (child in fast.elements) {
+			trace(child.name);
+			if(child.name == "layer"){
+				trace(child.node.data.innerData);
+			}
+		}
+
+
+
+		
+
+		var layerNodes = fast.nodes.layer;
+
+		
+
+		// for (i in 0 ... layerNodes.length) {
+		// 	trace(layerNodes.pop().data);
+		// }
+
+		// position = 0;
+		// var num = world.readU16LE(position); position += 2;
+		// var types = new Array<Int>();
+		// for (i in 0...num) { types.push(world.readU16LE(position)); position += 2; }
+		// var data1 = new Array<Int>();
+		// for (i in 0...num) { data1.push(world.readU16LE(position)); position += 2; }
+		// var data2 = new Array<Int>();
+		// for (i in 0...num) { data2.push(world.readU16LE(position)); position += 2; }
+		// var data3 = new Array<Int>();
+		// for (i in 0...num) { data3.push(world.readU16LE(position)); position += 2; }
+		
+		// Tile.tiles = new Array<kha2d.Tile>();
+		// for (i in 0...num) {
+		// 	Tile.tiles.push(new Tile(Assets.images.testMap, i, types[i], data1[i], data2[i], data3[i]));
+
+		// }
+
+
+		// entities
 		player = new Player();
 		bug = new Bug();
+
+		// tmx khapunk
+		// var tmxEntity:TmxEntity = new TmxEntity(Assets.blobs.testMap_tmx);
+		// tmxEntity.loadGraphic("testMap", ["RED", "GREEN"]);
+		// trace("tmxEntity", tmxEntity);
+		// tmxEntity.loadMask("collision", "solid");
+		// Scene.the.addBackgroundTilemap(tmxEntity); 
+
+
 
 		bug.x = 450;
 		bug.y = 36;
 
 		Scene.the.setColissionMap(null);
 
-		player.x = player.y = 36;
+		player.x = player.y = 36 + 72;
 		Scene.the.addHero(player);
 		Scene.the.addEnemy(bug);
 
@@ -60,6 +127,8 @@ class BranchNinja {
 	function render(framebuffer: Framebuffer): Void {
 		var g = framebuffer.g2;
 		g.begin();
+		// g.drawImage(Assets.images.bg, 0 ,0);
+		g.drawImage(Assets.images.testMap, -300 ,16);
 		Scene.the.render(g);
 		g.end();
 
